4be17b00420a465beaf625265180c07d700b9960
Eric Peterson <ericpeterson91@live.ca>
2022-07-01 17:53:52 -0600

    local storage bug

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "this is a note",
-      date: "01/01/2021",
-    },
-    {
-      id: nanoid(),
-      text: "this is a note",
-      date: "01/01/2021",
-    },
-    {
-      id: nanoid(),
-      text: "this is anotha one",
-      date: "01/01/2021",
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
 
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState("");
 
   const addNote = (text) => {
-    const date = new Date()
+    const date = new Date();
     const newNote = {
       id: nanoid(),
       text: text,
-      date: date.toLocaleDateString()
-    }
+      date: date.toLocaleDateString(),
+    };
 
-    const newNotes = [...notes, newNote]
-    setNotes(newNotes)
-  }
-  
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
   const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id)
-    setNotes(newNotes)
-  }
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  useEffect(() => {
+    const savedNotes = localStorage.getItem('react-notes-app-data')
+    
+    if(savedNotes !== null) {
+      setNotes(JSON.parse(savedNotes))
+    }
+  }, [])
+
+  useEffect(() => {
+    if(!!notes) {
+    localStorage.setItem('react-notes-app-data', JSON.stringify(notes));
+    }
+  }, [notes]);
+
+  
+
+ 
 
   return (
     <div className="container">
-      <Search handleSearchNote={setSearchText}/>
-      <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote} />
+      <Search handleSearchNote={setSearchText} />
+      <NotesList
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 };
