bebdc78f368532524c95235c2f204950701baf66
Eric Peterson <ericpeterson91@live.ca>
2022-07-02 22:09:13 -0600

    search input box

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const Search = ({handleSearchNote}) => {
   return (
     <div className='search'>
         <MdSearch className='search-icons' size='1.3em' />
-        <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder='search here...' />
+        <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder='search for notes here...' />
     </div>
   )
 }
